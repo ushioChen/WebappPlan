6246bc9a5744d934b9c9da7421fad1de1c5fb954
ushioChen <1072085644@qq.com>
2020-09-22 15:51:00 +0800

    added the Sudent class

--- Student.php
+++ Student.php
@@ -12,5 +12,29 @@
  * @author Ushio
  */
 class Student {
-    //put your code here
+    function __construct() {
+    $this->surname = '';
+    $this->first_name = '';
+    $this->emails = array();
+    $this->grades = array();
+}
+
+function add_email($which,$address) {
+
+$this->emails[$which] = $address;
+}
+function add_grade($grade) {
+$this->grades[] = $grade;
+}
+
+function toString() {
+$result = $this->first_name . ' ' . $this->surname;
+$result .= ' ('.$this->average().")\n";
+foreach($this->emails as $which=>$what)
+$result .= $which . ': '. $what. "\n";
+$result .= "\n";
+return '<pre>'.$result.'</pre>';
+
+}
+
 }
